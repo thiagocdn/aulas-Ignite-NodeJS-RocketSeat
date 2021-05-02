1d9be5db227f1363ac910b20874a92e9828a3f53
Thiago Cedran Santos <thiagocdn@gmail.com>
2021-05-02 13:34:01 -0300

    Criado middleware de validacao de usuario

--- chapter-i-primeiro-projeto-com-nodejs/README.md
+++ chapter-i-primeiro-projeto-com-nodejs/README.md
@@ -5,7 +5,7 @@
 ### Requisitos
 
 - [x] Deve ser possível criar uma conta
-- [] Deve ser possível buscar o extrato bancário do cliente
+- [x] Deve ser possível buscar o extrato bancário do cliente
 - [] Deve ser possível realizar um depósito
 - [] Deve ser possível realizar um saque
 - [] Deve ser possível buscar o extrato bancário do cliente por data
@@ -18,7 +18,7 @@
 ### Regras de negócio
 
 - [x] Não deve ser possível cadastrar uma conta com CPF já existente
-- [] Não deve ser possível fazer depósito em uma conta não existente
+- [x] Não deve ser possível fazer depósito em uma conta não existente
 - [] Não deve ser possível buscar extrato em uma conta não existente
 - [] Não deve ser possível fazer saque em uma conta não existente
 - [] Não deve ser possível excluir uma conta não existente
--- chapter-i-primeiro-projeto-com-nodejs/src/index.js
+++ chapter-i-primeiro-projeto-com-nodejs/src/index.js
@@ -7,12 +7,20 @@ app.use(express.json());
 
 const customers = [];
 
-/**
- * cpf  - string
- * name - string
- * id   - uuid
- * statement []
-*/
+//Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if(!customer)
+    return response.status(400).json({ error: 'Customer not found!' });
+
+  request.customer = customer;
+  return next();
+
+}
+
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
 
@@ -34,14 +42,9 @@ app.post('/account', (request, response) => {
   return response.status(201).send();
 });
 
-app.get('/statement/', (request, response) => {
-  const { cpf } = request.headers;
-
-  const customer = customers.find(customer => customer.cpf === cpf);
-
-  if(!customer)
-    return response.status(400).json({ error: 'Customer not found!' })
-
+app.get('/statement/', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  
   return response.json(customer.statement);
 });
 
