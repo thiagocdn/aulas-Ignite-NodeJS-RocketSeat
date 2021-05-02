d5559fd0f937e3ced82299297f9688aff1ea3452
Thiago Cedran Santos <thiagocdn@gmail.com>
2021-05-02 16:19:29 -0300

    Criado rota de atualizacao de conta

--- chapter-i-primeiro-projeto-com-nodejs/README.md
+++ chapter-i-primeiro-projeto-com-nodejs/README.md
@@ -8,7 +8,7 @@
 - [x] Deve ser possível buscar o extrato bancário do cliente
 - [x] Deve ser possível realizar um depósito
 - [x] Deve ser possível realizar um saque
-- [] Deve ser possível buscar o extrato bancário do cliente por data
+- [x] Deve ser possível buscar o extrato bancário do cliente por data
 - [] Deve ser possível atualizar dados da conta do cliente
 - [] Deve ser possível obter dados da conta do cliente
 - [] Deve ser possível deletar uma conta
--- chapter-i-primeiro-projeto-com-nodejs/src/index.js
+++ chapter-i-primeiro-projeto-com-nodejs/src/index.js
@@ -113,6 +113,21 @@ app.get('/statement/date', verifyIfExistsAccountCPF, (request, response) => {
   return response.json(statement);
 });
 
+app.put('/account', verifyIfExistsAccountCPF, (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(201).send();
+});
+
+app.get('/account', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer);
+})
+
 app.listen(3333, () => {
   console.log('Server is up!')
 });
