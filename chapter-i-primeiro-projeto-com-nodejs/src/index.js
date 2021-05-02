2524763cd7974313b5588390c988ee23b56a4526
Thiago Cedran Santos <thiagocdn@gmail.com>
2021-05-02 13:06:49 -0300

    CPF register checking

--- chapter-i-primeiro-projeto-com-nodejs/src/index.js
+++ chapter-i-primeiro-projeto-com-nodejs/src/index.js
@@ -15,12 +15,19 @@ const customers = [];
 */
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
-  const id = uuidv4();
+
+  const customerAlreadyExistis = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if(customerAlreadyExistis) {
+    return response.status(400).json({ error: "Customer already exists" })
+  }
 
   customers.push({
     cpf,
     name,
-    id,
+    id: uuidv4(),
     statement: []
   });
 
