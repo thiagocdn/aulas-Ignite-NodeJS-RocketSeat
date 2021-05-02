ca37600c6aa6096f06931e72ab01dff7b29f61d3
Thiago Cedran Santos <thiagocdn@gmail.com>
2021-05-02 13:03:59 -0300

    Create Account

--- chapter-i-primeiro-projeto-com-nodejs/src/index.js
+++ chapter-i-primeiro-projeto-com-nodejs/src/index.js
@@ -1,7 +1,31 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+
 app.use(express.json());
+
+const customers = [];
+
+/**
+ * cpf  - string
+ * name - string
+ * id   - uuid
+ * statement []
+*/
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body;
+  const id = uuidv4();
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  });
+
+  return response.status(201).send();
+});
 
 app.listen(3333, () => {
   console.log('Server is up!')
