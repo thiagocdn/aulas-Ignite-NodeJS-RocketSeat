133aa9ca48288167539eaf2725e50c500e6ac253
Thiago Cedran Santos <thiagocdn@gmail.com>
2021-05-02 13:38:26 -0300

    Criado rota de deposito

--- chapter-i-primeiro-projeto-com-nodejs/src/index.js
+++ chapter-i-primeiro-projeto-com-nodejs/src/index.js
@@ -42,10 +42,27 @@ app.post('/account', (request, response) => {
   return response.status(201).send();
 });
 
+// app.use(verifyIfExistsAccountCPF);
+
 app.get('/statement/', verifyIfExistsAccountCPF, (request, response) => {
   const { customer } = request;
-  
+
   return response.json(customer.statement);
+});
+
+app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: 'credit'
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
 });
 
 app.listen(3333, () => {
