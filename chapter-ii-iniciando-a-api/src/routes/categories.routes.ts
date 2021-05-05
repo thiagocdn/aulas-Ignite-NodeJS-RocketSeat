8c19c92a1652ccaa9d02a09cc32648bf179743bb
Thiago Cedran Santos <thiagocdn@gmail.com>
2021-05-04 21:14:37 -0300

    Adicionado UUID como id da categoria

--- chapter-ii-iniciando-a-api/src/routes/categories.routes.ts
+++ chapter-ii-iniciando-a-api/src/routes/categories.routes.ts
@@ -1,15 +1,20 @@
 import { Router } from 'express';
+import { v4 as uuidV4 } from 'uuid';
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post('/categories', (request, response) => {
+categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
   
-  categories.push({
-    name, description
-  });
+  const category = {
+    name,
+    description,
+    id: uuidV4(),
+  }
+
+  categories.push(category);
   
   return response.status(201).send();
 });
--- chapter-ii-iniciando-a-api/src/server.ts
+++ chapter-ii-iniciando-a-api/src/server.ts
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(categoriesRoutes);
+app.use('/categories', categoriesRoutes);
 
 app.listen(3333, () => {
   console.log('Server is up! :D')
