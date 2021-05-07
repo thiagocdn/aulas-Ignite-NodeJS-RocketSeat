be7252d3c92392e6415affa38b52add320653529
Thiago Cedran Santos <thiagocdn@gmail.com>
2021-05-06 23:05:29 -0300

    Ajustado arquivo de routes

--- chapter-ii-iniciando-a-api/src/routes/categories.routes.ts
+++ chapter-ii-iniciando-a-api/src/routes/categories.routes.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   return createCategoryController.handle(request, response);
