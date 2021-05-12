6d69b633f04a73e189e9bbc224ba4c3511799c81
Thiago Cedran Santos <thiagocdn@gmail.com>
2021-05-12 19:00:33 -0300

    Configurado Multer

--- chapter-ii-iniciando-a-api/src/routes/categories.routes.ts
+++ chapter-ii-iniciando-a-api/src/routes/categories.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: './tmp',
+})
 
 categoriesRoutes.post('/', (request, response) => {
   return createCategoryController.handle(request, response);
@@ -10,6 +15,12 @@ categoriesRoutes.post('/', (request, response) => {
 
 categoriesRoutes.get('/', (request, response) => {
   return listCategoriesController.handle(request, response);
-})
+});
+
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
+  const { file } = request;
+  console.log(file);
+  return response.send();
+});
 
 export { categoriesRoutes };
