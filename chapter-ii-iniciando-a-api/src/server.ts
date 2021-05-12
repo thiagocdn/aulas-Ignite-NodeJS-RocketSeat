3370c282ccdbc2b14cad9156dd3088958bbe0092
Thiago Cedran Santos <thiagocdn@gmail.com>
2021-05-12 18:36:43 -0300

    Criado arquivo de rotas

--- chapter-ii-iniciando-a-api/src/server.ts
+++ chapter-ii-iniciando-a-api/src/server.ts
@@ -1,13 +1,11 @@
 import express from 'express';
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationRoutes } from './routes/specifications.routes';
+import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/categories', categoriesRoutes);
-app.use('/specifications', specificationRoutes);
+app.use(router);
 
 app.listen(3333, () => {
   console.log('Server is up! :D')
