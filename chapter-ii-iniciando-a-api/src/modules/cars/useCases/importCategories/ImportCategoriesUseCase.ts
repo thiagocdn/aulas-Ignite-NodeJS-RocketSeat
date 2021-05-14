690a1dbd11945e07350ac05d4a797da065aff4ac
Thiago Cedran Santos <thiagocdn@gmail.com>
2021-05-13 22:10:19 -0300

    Delete do arquivo tmp

--- chapter-ii-iniciando-a-api/src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
+++ chapter-ii-iniciando-a-api/src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
@@ -29,6 +29,7 @@ class ImportCategoriesUseCase {
           description
         });
       }).on("end", () => {
+        fs.promises.unlink(file.path);
         resolve(categories);
       }).on("error", (err) => {
         reject(err);
