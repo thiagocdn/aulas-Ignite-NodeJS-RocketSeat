909dd010cdb931c16adb74afe27ddd2f0de77d28
Thiago Cedran Santos <thiagocdn@gmail.com>
2021-05-13 22:07:33 -0300

    Criado a doc swagger

--- chapter-ii-iniciando-a-api/src/server.ts
+++ chapter-ii-iniciando-a-api/src/server.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+
+import swaggerFile from './swagger.json';
+
 import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(router);
 
