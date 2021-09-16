29e90f516c71abbadb0e89307e73e95dccce0d2c
Thiago Cedran Santos <thiago.cedran@doji.co>
2021-09-16 17:07:39 -0300

    Added token validation for signin

---
+++ chapter-iii-continuando-a-aplicacao/src/routes/authenticate.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { AuthenticateUserController } from "../modules/accounts/useCases/authenticateUser/AuthenticateUserController";
+
+const authenticateRoutes = Router();
+
+const authenticateUserController = new AuthenticateUserController();
+
+authenticateRoutes.post("/sessions", authenticateUserController.handle);
+
+export { authenticateRoutes };
--- chapter-iii-continuando-a-aplicacao/src/routes/index.ts
+++ chapter-iii-continuando-a-aplicacao/src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { authenticateRoutes } from "./authenticate.routes";
 
 import { categoriesRoutes } from "./categories.routes";
 import { specificationRoutes } from "./specifications.routes";
@@ -9,5 +10,6 @@ const router = Router();
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationRoutes);
 router.use("/users", usersRoutes);
+router.use(authenticateRoutes);
 
 export { router };
