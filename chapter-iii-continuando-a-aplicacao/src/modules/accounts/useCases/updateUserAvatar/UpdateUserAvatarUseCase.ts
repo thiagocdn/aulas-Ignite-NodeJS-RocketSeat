5d41ece141b21fad88c9e91312fa344c062e82f9
Thiago Cedran Santos <thiago.cedran@doji.co>
2021-11-06 12:56:53 -0300

    Added deletion of avatar

--- chapter-iii-continuando-a-aplicacao/src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ chapter-iii-continuando-a-aplicacao/src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { deleteFile } from "../../../../utils/file";
 import { IUsersRepository } from "../../repositories/IUserRepository";
 
 interface IRequest {
@@ -15,6 +16,10 @@ class UpdateUserAvatarUseCase {
 
   async execute({ user_id, avatar_file }: IRequest): Promise<void> {
     const user = await this.usersRepository.findById(user_id);
+
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
+    }
     user.avatar = avatar_file;
     await this.usersRepository.create(user);
   }
